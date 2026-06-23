102b934fc1669e16b9dc3d8023da33207fa5e5ba
David O'Reilly <git@dsoreilly.me>
2026-06-23 14:33:22 +0100

    extend the minigrep proj

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,9 +6,7 @@ use std::process;
 use minigrep::{search, search_case_insensitive};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -26,13 +24,18 @@ struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -47,13 +50,13 @@ impl Config {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
+    let search_iter: Box<dyn Iterator<Item = &str>> = if config.ignore_case {
+        Box::new(search_case_insensitive(&config.query, &contents))
     } else {
-        search(&config.query, &contents)
+        Box::new(search(&config.query, &contents))
     };
 
-    for line in results {
+    for line in search_iter {
         println!("{line}");
     }
 
